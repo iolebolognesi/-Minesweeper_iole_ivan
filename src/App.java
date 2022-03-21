114be52c03da92abd997c466d6299ae92cbfd22c
DESKTOP-O46DQUM\Lenovo <ivandeyanov.ivanov@student.kuleuven.be>
2022-03-21 09:57:09 +0100

    strange

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 import model.Difficulty;
 //import model.Minesweeper;
+import model.Minesweeper;
 import model.PlayableMinesweeper;
 import view.MinesweeperView;
 
@@ -10,14 +11,15 @@ public class App
     {
         MinesweeperView view = new MinesweeperView();
         //Uncomment the lines below once your game model code is ready; don't forget to import your game model 
-        //PlayableMinesweeper model = new Minesweeper();
+        PlayableMinesweeper model = new Minesweeper();
         
         
         /**
             Your code to bind your game model to the game user interface
-        */
+
+        **/
         
         
-        //model.startNewGame(Difficulty.EASY);
+        model.startNewGame(Difficulty.EASY);
     }
 }
